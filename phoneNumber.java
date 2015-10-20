0b105513d149f85ec3f89522490f8b02cd5ab486
Q <Qrules@users.noreply.github.com>
2015-10-20 16:00:30 -0500

    Update

--- phoneNumber.java
+++ phoneNumber.java
@@ -3,10 +3,10 @@ public class phoneNumber { //Define the class
 	public static void main(String[] args) { //Begin main
 		Scanner scan = new Scanner(System.in); //Starts a scanner that will handle input
 		
-		System.out.print("Please enter a Name: "); //Prompts user for input
-		String name = scan.next(); //Saves input as a string (their name)
 		System.out.print("Please enter a phone number: "); //Prompts user for input
 		String rawNumber = scan.nextLine(); //Saves raw phone number as string to be broken down later
+		System.out.print("Please enter a Name: "); //Prompts user for input
+		String name = scan.next(); //Saves input as a string (their name)
 		String temp = ""; //Define temporary string to be used later
 		String newNumber = ""; //Define string for the purified phone number
 		
@@ -19,7 +19,7 @@ public class phoneNumber { //Define the class
 			}
 		}
 		
-		if(newNumber.length() != 10 || newNumber.length() != 7) //Make sure the number is the correct length with/without the area code
+		if(newNumber.length() != 10 && newNumber.length() != 7) //Make sure the number is the correct length with/without the area code
 		{
 			System.out.println("This is no phone number!!!"); //If it is not a phone number, tell the user and exit the program
 			System.exit(0);
